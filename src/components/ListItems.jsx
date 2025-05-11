b381a3b87b9715bfa1bff3d4464fd323bbfdc9a1
alex1987bystrov <alex_bystrov_1987@mail.ru>
2025-05-11 19:31:00 +0300

    fixed nav-panel

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -2,13 +2,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export default function ListItems(props){
-    const links = [
-        {id:1, link:"#home-top"},
-        {id:2, link:"#about"},
-        {id:3, link:"#tours"},
-        {id:4, link:"#store"},
-        {id:5, link:"#contact"}
-    ];
+    const links = props.links;
     const nI = props.nOfItems;
     const nLI = props.nOfListItems;
     const index = props.index;
@@ -23,13 +17,12 @@ export default function ListItems(props){
     for(let i = 0; i < nLI; i++){
         items.push(allItems[index+i]);
     };
-
+    
     let listItems = items.map(
     item => item === items[1] ? 
     <li key = {uuidv4()}><a className = "nav-link active" href = {item.link}>{item.id}</a></li> : 
     <li key = {uuidv4()}><a className = "nav-link" href = {item.link}>{item.id}</a></li>
     );
-    console.log(listItems[1].props.children.props.href);
     return(
         <ul className = "nav-panel-by-number-list">{listItems}</ul>
     );
--- src/components/NavByNumber.jsx
+++ src/components/NavByNumber.jsx
@@ -3,18 +3,27 @@ import ListItems from "./ListItems";
 
 
 export default function NavByNumber({nOfItems, nOfListItems}){
-    const [index, setIndex] = useState(0);
+    const links = [
+        {id:1, link:"#home-top"},
+        {id:2, link:"#about"},
+        {id:3, link:"#tours"},
+        {id:4, link:"#store"},
+        {id:5, link:"#contact"}
+    ];
+    const [index, setIndex] = useState(0);    
     function handleNext(){
         (index < nOfItems-1) ? setIndex(index => index + 1) : setIndex(0);
+        document.querySelector(links[(index < nOfItems-1) ? index+1 : 0].link).scrollIntoView({block:'center'});
     }
     function handlePrev(){
         (index > 0) ? setIndex(index => index - 1) : setIndex(nOfItems-1);
+        document.querySelector(links[(index > 0) ? index-1 : nOfItems-1].link).scrollIntoView({block:'center'});
     }    
     return(
         <div className = "nav-by-number">
             <nav id = "nav-panel-by-number">
                 <div className = "arr-left" onClick = {handlePrev}></div>
-                <ListItems nOfListItems = {nOfListItems} nOfItems = {nOfItems} index = {index} />
+                <ListItems nOfListItems = {nOfListItems} nOfItems = {nOfItems} index = {index} links = {links}/>
                 <div className = "arr-right" onClick = {handleNext}></div>
             </nav>
         </div>
