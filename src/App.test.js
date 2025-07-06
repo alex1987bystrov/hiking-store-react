4d20bfa01ce57473071b23aaf84741ab148de758
alex1987bystrov <alex_bystrov_1987@mail.ru>
2025-07-06 18:47:21 +0300

    add simple tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import Home from './components/Home';
+import About from './components/About';
+import Tours from './components/Tours';
+import Store from './components/Store';
+import Contact from './components/Contact';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders App', () => {
+  render(<App />);
+  const linkElement = screen.getByText('Home');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Home', () => {
+  render(<Home />);
+  const linkElement = screen.getByText('Home');
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders About', () => {
+  render(<About />);
+  const linkElement = screen.getByText('ABOUT US');
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders Tours', () => {
+  render(<Tours />);
+  const linkElement = screen.getByText('CHOOSE');
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders Store', () => {
+  render(<Store />);
+  const linkElement = screen.getByText('STORE');
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders Contact', () => {
+  render(<Contact />);
+  const linkElement = screen.getByText('CONTACT');
+  expect(linkElement).toBeInTheDocument();
+});
