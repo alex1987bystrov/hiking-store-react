41ed83d4352bc3a99e4d124e1c9152b4ee8c9a9d
alex1987bystrov <alex_bystrov_1987@mail.ru>
2025-07-11 21:23:44 +0300

    fix test home

--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -7,31 +7,36 @@ import Slider from "./sliderComponents/Slider";
 
 test('follow the link "Home" after clicking', () => {
     render(<App />);
-    const linkElement = screen.getByRole('link', { name: /Home/i })
-    fireEvent.click(linkElement);
-    expect(screen.getByAltText('logo')).toBeInTheDocument();
+    const linkHome = screen.getByRole('link', { name: /Home/i })
+    linkHome.addEventListener('click', () => {linkHome.classList.add('active')});
+    fireEvent.click(linkHome);
+    expect(linkHome).toHaveClass('active');
 });
 test('follow the link "About us" after clicking', () => {
     render(<App />);
-    const linkElement = screen.getByRole('link', { name: /About us/i });
-    fireEvent.click(linkElement);
-    expect(screen.getByText('ABOUT US')).toBeInTheDocument();
+    const linkAbout = screen.getByRole('link', { name: /About us/i });
+    linkAbout.addEventListener('click', () => {linkAbout.classList.add('active')});
+    fireEvent.click(linkAbout);
+    expect(linkAbout).toHaveClass('active');
 });
-// test('follow the link "Tours" after clicking', () => {
-//     render(<App />);
-//     const linkElement = screen.getByRole('link', { name: /Tours/i });
-//     fireEvent.click(linkElement);
-//     expect(screen.getByText('YOUR TOUR')).toBeInTheDocument();
-// });
-// test('follow the link "Store" after clicking', () => {
-//     render(<App />);
-//     const linkElement = screen.getByRole('link', { name: /Store/i });
-//     fireEvent.click(linkElement);
-//     expect(screen.getByText('STORE')).toBeInTheDocument();
-// });
-// test('follow the link "Contact" after clicking', () => {
-//     render(<App />);
-//     const linkElement = screen.getByRole('link', { name: /Contact/i });
-//     fireEvent.click(linkElement);
-//     expect(screen.getByText('CONTACT')).toBeInTheDocument();
-// });
+test('follow the link "Tours" after clicking', () => {
+    render(<App />);
+    const linkTours = screen.getByRole('link', { name: /Tours/i });
+    linkTours.addEventListener('click', () => {linkTours.classList.add('active')});
+    fireEvent.click(linkTours);
+    expect(linkTours).toHaveClass('active');
+});
+test('follow the link "Store" after clicking', () => {
+    render(<App />);
+    const linkStore = screen.getByRole('link', { name: /Store/i });
+    linkStore.addEventListener('click', () => {linkStore.classList.add('active')});
+    fireEvent.click(linkStore);
+    expect(linkStore).toHaveClass('active');
+});
+test('follow the link "Contact" after clicking', () => {
+    render(<App />);
+    const linkCont = screen.getByRole('link', { name: /Contact/i });
+    linkCont.addEventListener('click', () => {linkCont.classList.add('active')});
+    fireEvent.click(linkCont);
+    expect(linkCont).toHaveClass('active');
+});
